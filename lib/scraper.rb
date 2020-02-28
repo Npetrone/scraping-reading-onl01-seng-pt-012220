62b07208b5b68984355ebc4ccb8eea69115809b8
Nicholas Petrone <nickpetrone92@gmail.com>
2020-02-28 08:36:31 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,4 +16,4 @@ require 'open-uri'
 html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
  
-puts doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G").collect
