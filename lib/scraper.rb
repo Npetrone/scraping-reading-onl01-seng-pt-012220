a1367294939e1ed57183bec2cc115dbf901388b5
Nicholas Petrone <nickpetrone92@gmail.com>
2020-02-28 08:36:01 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,12 +1,19 @@
+# require 'nokogiri'
+# require 'open-uri'
+
+# html = open("https://flatironschool.com/")
+
+# Nokogiri::HTML(html)
+# doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+# puts doc.css(".headline-26OIBN").text
+
+
+
+
 require 'nokogiri'
 require 'open-uri'
-
+ 
 html = open("https://flatironschool.com/")
-
-Nokogiri::HTML(html)
-doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-puts doc.css(".headline-26OIBN").text
-
-
-
-
+doc = Nokogiri::HTML(html)
+ 
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
